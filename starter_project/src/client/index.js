15c59e163808333a47d85db7f749489f829cd274
vuongnd9 <nguyendinhvuong.act@gmail.com>
2024-04-28 14:56:53 +0700

    add .scss to index.js

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,12 +1,11 @@
 // js files
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
+import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-import './styles/resets.scss'
-
 
 
 // alert("I EXIST")
